e7df30b1a089ee62a4029324010f6b79036a0cfa
Colton Donkersgoed <coltonmdonk@gmail.com>
2024-01-20 17:42:35 -0500

    adjusted some unused code and titling

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-import os
-
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request
 from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
 
 
 # CONSTANT FOR GRADES
