0a7d92e5d5a930e5d292d589f38c5d10264957d3
Colton Donkersgoed <coltonmdonk@gmail.com>
2024-01-19 20:45:52 -0500

    multiple changes; see desc; results
    - removed apology parameters, will handle errors with flash instead
    - removed need for helpers
    - added ability to add course grades to students

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+
+# CONSTANT FOR GRADES
+GRADES = ['A', 'B', 'C', 'D', 'F']
 
 # Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-# Custom filter
-app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -42,7 +41,6 @@ def after_request(response):
 
 @app.route("/")
 def index():
-
     return render_template("index.html")
 
 
@@ -52,32 +50,24 @@ def register():
     """Register user"""
     """Log user in"""
 
-    # Forget any user_id
-    session.clear()
+    students = db.execute("SELECT * FROM students ORDER BY ID ASC")
+
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
-        # Ensure first name was submitted
-        if not request.form.get("firstname"):
-            return apology("must provide your first name", 400)
-
-        # Ensure last name was submitted
-        elif not request.form.get("lastname"):
-            return apology("must provide your last name", 400)
         
-
+        # pull attributes from the form
         firstname = request.form.get("firstname")
 
         lastname = request.form.get("lastname")
 
         birthdate = request.form.get("birthdate")
 
-        existing_students = db.execute("SELECT firstname FROM STUDENTS")
+        existing_students = db.execute("SELECT firstname, lastname FROM STUDENTS")
 
         for student in existing_students:
             if student["firstname"] == firstname and student["lastname"] == lastname:
-                return apology("current student already present in database")
+                return flash("current student already present in database", "warning")
 
         # Insert user into database
         db.execute("INSERT INTO students (firstname, lastname, birthdate) VALUES (?, ?, ?)", firstname, lastname, birthdate)
@@ -85,12 +75,14 @@ def register():
         # notify user that the student was registered
         flash("Student " + firstname + " " + lastname + " has been registered.")
 
+        students = db.execute("SELECT * FROM students ORDER BY ID ASC")
+
         # Redirect user to home page
-        return render_template("student.html")
+        return render_template("student.html", students=students)
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("student.html")
+        return render_template("student.html", students=students)
     
 
 @app.route("/courses", methods=["GET", "POST"])
@@ -98,15 +90,10 @@ def courses():
     """Register user"""
     """Log user in"""
 
-    # Forget any user_id
-    session.clear()
+    courses = db.execute("SELECT * FROM courses ORDER BY coursename DESC")
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("coursename"):
-            return apology("must provide your course name", 400)
         
         coursename = request.form.get("coursename")
 
@@ -114,15 +101,58 @@ def courses():
 
         for course in existing_courses:
             if course["coursename"] == coursename:
-                return apology("current course already present in database")
+                flash("Course is already in the database.", "warning")
+                return render_template("courses.html")
 
         # Insert user into database
         db.execute("INSERT INTO COURSES (coursename) VALUES (?)", coursename)
 
+        flash("Course " + coursename + " has been added to the registry.")
+
+        # Recalls the courses to add the newly added course
+        courses = db.execute("SELECT * FROM courses ORDER BY coursename DESC")
+
         # Redirect user to home page
-        return redirect("/")
+        return render_template("courses.html", courses=courses)
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("courses.html")
+        return render_template("courses.html", courses=courses)
+    
+@app.route("/results", methods=["GET", "POST"])
+def results():
+    """ method for launching the results page"""
+
+    courses = db.execute("SELECT coursename FROM COURSES")
+    students = db.execute("SELECT * FROM students") 
+
+    # user reached route via post, i.e. submitting the form
+    if request.method == "POST":
+        
+
+        # grab variables from the form
+        coursename = request.form.get("course")
+        studentid = request.form.get("student")
+        grade = request.form.get("grade")
+
+
+        required_student = db.execute("SELECT * FROM students WHERE ID=%s", studentid)
+
+        print(required_student)
+
+        
+
+
+        db.execute("INSERT INTO grades (coursename, firstname, lastname, grade) VALUES (?, ?, ?, ?)", 
+                   coursename, required_student[0]['firstname'], required_student[0]['lastname'], grade)
+
+
+        return render_template("results.html", students = students, courses = courses, grades=GRADES)
+
+
+    # user reached via get -- clicking link or redirct
+    else:
+        return render_template("results.html", students=students, courses=courses, grades=GRADES)
+
+           
 
---
+++ other data/sql commands.sql
@@ -0,0 +1,25 @@
+--SQLITE
+-- CREATE TABLE students (
+--     ID INTEGER PRIMARY KEY  AUTOINCREMENT,
+--     firstname TEXT      NOT NULL,
+--     lastname TEXT       NOT NULL,
+--     birthdate TEXT      NOT NULL
+-- );
+
+-- CREATE TABLE courses (
+--     ID INTEGER PRIMARY KEY AUTOINCREMENT,
+--     coursename  TEXT    NOT NULL,
+--     enrolled    INT DEFAULT 0
+-- )
+
+
+CREATE TABLE grades (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    coursename TEXT NOT NULL,
+    firstname TEXT  NOT NULL,
+    lastname TEXT   NOT NULL,
+    grade   TEXT    NOT NULL
+)
+
+
+DROP TABLE courses;
